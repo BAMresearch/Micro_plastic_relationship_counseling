f01476c093a5119033a3f1509ad8d26aa2f6dc73
jfrontze <julius.frontzek@bam.de>
2022-02-11 11:57:16 +0100

    Added logging and added the functionality to wait a specified amount of time before shutting down the client after an error is detected

--- src/client.py
+++ src/client.py
@@ -6,11 +6,15 @@ import time
 import datetime
 import websocket
 import os
+from log import Log
 
 class ClientData:
 
-    def __init__(self):
+    def __init__(self, log_name: str):
         """Initializes the client data."""
+        # Set up logger
+        self.log = Log(log_name)
+
         # Set up the ConfigParser. Used to read all the client data from the config.ini file
         config = configparser.ConfigParser()
         config.read('../config/config.ini')
@@ -25,6 +29,10 @@ class ClientData:
         self.confirm_reset_x_pos = int(config['CONFIRM_RESET_POSITION']['confirm_reset_x_pos'])
         self.confirm_reset_y_pos = int(config['CONFIRM_RESET_POSITION']['confirm_reset_y_pos'])
 
+        # Delay before shutting down the client after an error message has been received
+        self.client_shutdown_delay_mins = float(config['DELAY_TIME']['client_shutdown_delay_mins'])
+
+
 class UserWebsocketEngine:
     """Sets up websocket to talk to the server."""
 
@@ -37,14 +45,22 @@ class UserWebsocketEngine:
         def on_message(ws, message):
             """Function that processes all incoming messages except for error messages."""
             if message == "STATUS SERVER: Error detected":
-                print("Resetting the program.")
+
+                # Wait some time before shutting down the client
+                self.client_data.log.logger.debug(f"Waiting {self.client_data.client_shutdown_delay_mins} minutes for safety reasons before shutting down the client.")
+                time.sleep(self.client_data.client_shutdown_delay_mins * 60)
+
+                self.client_data.log.logger.debug("Resetting the program.")
                 # Click the reset button
                 pyautogui.click(self.client_data.reset_x_pos, self.client_data.reset_y_pos)
                 
                 # Wait a little and then press the reset confirmation button
                 time.sleep(2)
-                print("Confirming the program's reset.")
+                self.client_data.log.logger.debug("Confirming the program's reset.")
                 pyautogui.click(self.client_data.confirm_reset_x_pos, self.client_data.confirm_reset_y_pos)
+
+                # Remove logging handler to stop logging at this point
+                self.client_data.log.logger.removeHandler(self.client_data.log.handler)
 
                 # Send the server an acknolegement about the error message
                 ws.send("STATUS CLIENT: Error detected!")
@@ -54,11 +70,11 @@ class UserWebsocketEngine:
 
         def on_error(ws, error):
             """Function that handles websocket connection errors."""
-            print(error)
+            self.client_data.log.logger.debug(error)
 
         def on_close(ws):
             """Function that deals with the closure of the websocket connection."""
-            print('CONNECTION CLOSED AT: ' + str(datetime.now().time().strftime('%H:%M:%S')))
+            self.client_data.log.logger.debug('CONNECTION CLOSED AT: ' + str(datetime.now().time().strftime('%H:%M:%S')))
 
         def on_open(ws):
             """Function that deals with the opening of the websocket connection."""
@@ -88,16 +104,16 @@ def stay_connected(client_data: ClientData):
             time.sleep(1)
 
     except KeyboardInterrupt:
-        print("Program terminated due to keyboard interruption.")
+        client_data.log.logger.debug("Program terminated due to keyboard interruption.")
 
 
 def main():
-    """Initializes and runs the client."""
+    """Main method. Initializes and runs the client."""    
     # Change working directory to file path
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
     # Initialize the client data
-    client_data = ClientData()
+    client_data = ClientData('client')
 
     # Run the client in an asyncio loop
     stay_connected(client_data)
